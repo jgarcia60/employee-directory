82ce053037e483f35e9133339840816e3a9965da
jgarcia60 <68722324+jgarcia60@users.noreply.github.com>
2021-01-23 20:07:56 -0500

    finished onChange function and styling

--- src/App.js
+++ src/App.js
@@ -1,16 +1,87 @@
+import React, {Component} from 'react';
 import './App.css';
 import Title from './components/Title/index';
 import Filter from '../src/components/Filter/index'; 
 import Wrapper from '../src/components/Wrapper/index';
 import Head from '../src/components/Head/index';
 import Row from '../src/components/Row/index';
+import API from './Utils/API';
 
 
-function App() {
+class App extends Component {
+
+  state = {
+    users: [],
+    searchUsers: [],
+    search: "",
+    sorted: "ascending"
+  };
+
+  componentDidMount() {
+    this.generateRandomUsers();
+  };
+
+  generateRandomUsers = () => {
+    API.getRandomUsers()
+      .then((res) => {
+      console.log(res)
+        this.setState({
+          users: res.data.results,
+          searchUsers: res.data.results,
+        })
+      })
+      .catch((err) => console.log(err));
+  };
+
+  handleInputChange = (e) => {
+    const employeeName = e.target.value;
+
+    if(this.state.search === ""){
+      this.setState({
+        users: this.state.users,
+      })
+    }
+  
+    const searchUsers = this.state.users.filter((employees) => {
+    const { first: firstName, last: lastName } = employees.name;
+    const searchedEmployeesName = `${firstName} ${lastName}`;
+    return searchedEmployeesName
+      .toLowerCase()
+      .includes(employeeName.toLowerCase().trim());
+      //check to match employee names with user input
+  });
+  this.setState({
+    searchUsers: searchUsers,
+    search: employeeName,
+  });
+};
+
+  handleSortByName = () => {
+    const sortEl = this.state.searchUsers;
+    //sorts list of names based on ascending or descending
+    if (this.state.sorted === "ascending") {
+      const sorted = sortEl.sort((a, b) =>
+        a.name.first > b.name.first ? 1 : -1
+      );
+      this.setState({
+        searchUsers: sorted,
+        sorted: "descending",
+      });
+    } else {
+      const sorted = sortEl.sort((a, b) =>
+        a.name.first > b.name.first ? -1 : 1
+      );
+      this.setState({
+        searchUsers: sorted,
+        sorted: "ascending",
+      });
+    }
+  }
+render() {
   return (
     <Wrapper>
         <Title>Employee Directory
-          <Filter inputChanged={this.handleInputChange} />
+          <Filter handleInputChange={this.handleInputChange} />
         </Title>
         <Head sortByName={this.handleSortByName} />
         {this.state.searchUsers.map((user) => (
@@ -21,10 +92,12 @@ function App() {
             age={user.dob.age}
             phone={user.phone}
             src={user.picture.large}
+            key={user.login.username} //react asked for a key so i just gave it a username
           />
         ))}
       </Wrapper>
   );
+  }
 }
 
 export default App;
--- src/components/Filter/index.js
+++ src/components/Filter/index.js
@@ -3,13 +3,14 @@ import "./style.css";
 
 function Filter(props) {
   return (
-    <div className="filter" onChange={props.inputChanged}>
+    <div className="filter" >
       <div className="form-group form-inline">
         <input
           type="search"
           className="form-control"
           id="searchForm"
           placeholder="Search Users"
+          onChange={props.handleInputChange}
         ></input>
       </div>
     </div>
